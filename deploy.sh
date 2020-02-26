1f8c7dfba7acbcd75bcbd18831d7fd26d85859b9
Zdenek Vales <zdenek.vales@yoso.fi>
2020-02-26 13:14:30 +0100

    Deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-activemqDir=""
+activemqDir="/home/zdenda/tools/activemq/apache-activemq-5.15.11"
 
+# copy jar with plugin
 cp target/ActiveMQPluginTest-1.0-SNAPSHOT.jar "${activemqDir}/lib"
 
-copy conf/activemq.xml "${activemqDir}/conf"
+# copy configuration
+cp -r deploy/* "${activemqDir}"
